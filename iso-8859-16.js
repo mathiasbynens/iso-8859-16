47f6d61bc00286b44bb764a52fbd5c6890124ace
Mathias Bynens <mathias@qiwi.be>
2021-07-30 13:04:57 +0200

    Release v1.1.0

--- iso-8859-16.js
+++ iso-8859-16.js
@@ -1,4 +1,4 @@
-/*! https://mths.be/iso-8859-16 v1.0.2 by @mathias | MIT license */
+/*! https://mths.be/iso-8859-16 v1.1.0 by @mathias | MIT license */
 ;(function() {
 
 	const stringFromCharCode = String.fromCharCode;
@@ -94,7 +94,7 @@
 		labels: [
 			'iso-8859-16'
 		],
-		version: '1.0.2',
+		version: '1.1.0',
 	};
 
 	module.exports = iso885916;
